485aca000601664678caa7bcf95fe8db6650283a
akibahmed229 <akib4418@gmail.com>
2024-06-24 19:18:24 +0600

    initial

---
+++ config/default.nix
@@ -0,0 +1,17 @@
+{
+# Import all your configuration modules here
+  imports = [
+  ./core
+  ./ui
+  ./lsp
+  ./telescope
+  ./treesitter
+  ./bufferlines
+  ./statusline
+  ./file-tree
+  ./git
+  ./none-ls
+  ./snippets
+  ./completion
+  ];
+}
